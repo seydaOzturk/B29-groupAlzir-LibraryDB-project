c4862ecca4b518ffa7a5c9a5480cb96076a79518
seydaOzturk <aslanseyda91@gmail.com>
2023-08-10 11:04:33 -0400

    updated DashBoardPage

--- src/test/java/com/LibraryDB/pages/DashboardPage.java
+++ src/test/java/com/LibraryDB/pages/DashboardPage.java
@@ -5,7 +5,7 @@ import org.openqa.selenium.By;
 import org.openqa.selenium.WebElement;
 import org.openqa.selenium.support.FindBy;
 
-public class DashboardPage {
+public class DashboardPage extends BasePage{
     @FindBy(id = "borrowed_books")
     public WebElement borrowedBooksNumber;
 
