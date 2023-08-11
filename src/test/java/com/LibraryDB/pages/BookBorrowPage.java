d1188f2adc90282e369a65310a91edb301a5af17
Korhanguvn <korhanguvn@gmail.com>
2023-08-11 18:44:06 -0400

    all done :D

---
+++ src/test/java/com/LibraryDB/pages/BookBorrowPage.java
@@ -0,0 +1,35 @@
+package com.LibraryDB.pages;
+
+import com.LibraryDB.utility.Driver;
+import org.openqa.selenium.WebElement;
+import org.openqa.selenium.support.FindBy;
+import org.openqa.selenium.support.PageFactory;
+
+import java.util.List;
+
+public class BookBorrowPage {
+
+
+    public BookBorrowPage(){PageFactory.initElements(Driver.getDriver(),this);}
+
+
+    @FindBy(xpath = "//td[text()='The Lion']//parent::tr//td//a")
+    public WebElement borrowBookBtn;
+
+    @FindBy(xpath = "//tr//td[2]")
+    public List<WebElement> allBorrowedBooksName;
+
+    @FindBy(xpath = "//td[text()='The Lion']//parent::tr//td[6]")
+    public WebElement returnCell;
+
+    @FindBy(xpath = "//*[@id=\"menu_item\"]/li[2]/a")
+    public WebElement borrowingBooks;
+
+    @FindBy(xpath = "//td[text()='The Lion']/parent::tr//td//a")
+    public List<WebElement> returnBookBtns;
+
+
+
+
+
+}
